1b1e6eb9b1f66bd54c30baa8b179a827fb806754
dwu <dwu@github-desktop>
2019-10-10 09:41:30 +0800

    Updates

---
+++ precache-manifest.80a42351fe21acdd4260d7ecafc6c61a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b31c752945ed298eb6e",
+    "url": "/react-tutorial/static/css/main.c062b768.chunk.css"
+  },
+  {
+    "revision": "7b31c752945ed298eb6e",
+    "url": "/react-tutorial/static/js/main.7b31c752.chunk.js"
+  },
+  {
+    "revision": "f113653d912d24231947",
+    "url": "/react-tutorial/static/js/1.f113653d.chunk.js"
+  },
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "1fc0ba684d112394f952c8aec79331f6",
+    "url": "/react-tutorial/index.html"
+  }
+];
